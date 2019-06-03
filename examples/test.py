c23e3a786b75220d27e9afcd91b562acee5cfcf5
issaclin32 <issaclin32@gmail.com>
2019-06-03 16:29:41 +0800

    first commit

---
+++ examples/test.py
@@ -0,0 +1,15 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'\\..')
+import numpy as np
+import cv2
+import cvttf
+
+im = cv2.imread('.\\koala.jpg')
+im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+test_text = """English: koala / Chinese: 無尾熊 / Japanese: コアラ 
+/ Vietnamese: gấu túi / Hindu: कोआला/ Thai: โคอาลา """
+cvttf.putText(im, '', (10, 10), None, 0.7)
+
+cv2.imshow('', im)
+cv2.waitKey(0)
