d21f7bc825e0df1243715d23c33c442262f4d80f
issaclin32 <issaclin32@gmail.com>
2019-06-04 14:27:29 +0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,40 @@
 import os
 import setuptools
-from pprint import pprint
-from setuptools.command.develop import develop
 from setuptools.command.install import install
+from pkg_resources import DistributionNotFound, get_distribution
+from distutils.sysconfig import get_python_lib
+import urllib.request as req  # urlretrieve
+import zipfile
 
 #with open("README.md", "r") as fh:
 #    long_description = fh.read()
 
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
+
+# Using post-installation commands to download font files since PyPI does not support packages larger than 60MB.
+class RedefinedInstallCommand(install):
     def run(self):
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        develop.run(self)
+        install.run(self)  # run installation
 
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        install.run(self)
+        # --- post-installation commands ---
+        print('Downloading font files from Github')
 
-font_dir = 'cvttf/fonts'
-font_files = {}
+        # not using cvttf.__file__ since there is a "cvttf" folder under current path
+        CVTTF_ROOT_PATH = get_python_lib()+'\\cvttf'  # under site-packages
+        if not os.path.isdir(CVTTF_ROOT_PATH):
+            raise FileNotFoundError(f'path "{CVTTF_ROOT_PATH}" cannot be found. CVTTF is probably not correctly installed.')
 
-for dirName, subDirs, files in os.walk(font_dir):
-    for file in files:
-        if os.path.isfile(dirName+'/'+file):
-            dirName = dirName.replace('\\', '/')
-            if dirName in font_files:
-                font_files[dirName].append(dirName+'/'+file)
-            else:
-                font_files[dirName] = [dirName+'/'+file]
+        input()
+
+def get_dist(package_name):
+    try:
+        return get_distribution(package_name)
+    except DistributionNotFound:
+        return None
+
+
+install_requires=['numpy', 'Pillow', 'fontTools']
+if get_dist('opencv_python') is None and get_dist('opencv_contrib_python') is None:
+    install_requires.append('opencv_contrib_python')
 
 
 setuptools.setup(
@@ -42,17 +47,13 @@ setuptools.setup(
     # long_description_content_type="text/markdown",
     url="https://github.com/issaclin32/CVTTF",
     packages=['cvttf'],
-    install_requires=['numpy', 'Pillow', 'fontTools'],
-    # not using data_dir parameter due to Anaconda compatibility issues
-    data_files=font_files.items(),
-    include_package_data=True,
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
+        'install': RedefinedInstallCommand,
     },
 )
