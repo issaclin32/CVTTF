30f14104a58f58b80bfb3de5d9658d3dd0794adc
issaclin32 <issaclin32@gmail.com>
2019-06-12 17:21:51 +0800

    modify font class

--- cvttf/font_class.py
+++ cvttf/font_class.py
@@ -42,13 +42,12 @@ class FontSet:
         return f'FontSet({str(self.font_paths)})'
 
 
-
 # use only 3 sizes for TTF: 12pt, 32pt, 96pt
 # resizing image to draw fonts sizes between them
 class FontFamily:
     def __init__(self, TTF_path: Union[str, List[FontSet]], size: int = 32):
-        self._char_range_combined = {}
-        self._char_range_for_each_fontset = []
+        self._char_range_combined: Dict[str, int] = {}
+        self._char_range_for_each_fontset: List[set] = []
         if type(TTF_path) is str:
             if not os.path.isfile(TTF_path):
                 raise FileNotFoundError(f'TTF file: {TTF_path} does not exist.')
@@ -60,19 +59,39 @@ class FontFamily:
             for index_fontset, fontset in enumerate(reversed(TTF_path)):
                 supported_range = find_supported_range(list(fontset.font_paths.values())[0])  # if both "regular" and "bold" are provided, check "regular" only
                 self._char_range_combined.update( zip(supported_range, [len(TTF_path)-index_fontset-1]*len(supported_range)) )
-                self._char_range_for_each_fontset.append(supported_range)
+                self._char_range_for_each_fontset.append(set(supported_range))
         else:
             raise TypeError('TTF path can only be "str" or "List[FontSet]"')
 
-        def find_fontset(text: str):
-            """
-            return an integer if the "text" can be displayed using only one charset (e.g. NotoSans Thai)
-            otherwise, return a list of integer which shows what charset should be chosen for each character
-            e.g. ETF基金 -> [0,0,0,4,4]
-            """
-            return
-
         return
+
+    def find_fontset(self, text: str, allow_unsupported_fonts: bool = True) -> Union[int, List[int]]:
+        """
+        return an integer if "text" can be displayed using only one charset (e.g. NotoSans Thai)
+        otherwise, return a list of integer which shows what charset should be chosen for each character
+        e.g. ETF基金 -> [0,0,0,4,4]
+        if there are some characters that are not supported by every FontSet in this FontFamily,
+        then, put 0 in corresponding position (if "allow_supported_fonts" is set to True)
+        or raise Exception (if "allow_supported_fonts" is set to False)
+        """
+
+        # check if "text" can be displayed using only one charset
+        st = set(text)
+        for i, r in enumerate(self._char_range_for_each_fontset):
+            if st in r:
+                return i
+
+        # if not, check each character
+        ret = []
+        for c in text:
+            if c in self._char_range_combined:
+                ret.append(self._char_range_combined[c])
+            else:
+                if allow_unsupported_fonts:
+                    ret.append(0)
+                else:
+                    raise ValueError(f'character "{c}" is not supported in this font family.')
+        return ret
 
 
 class Font:
